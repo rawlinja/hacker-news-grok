2c6a256b047486467e4ddfd79e26334e608e9b03
Jermaine Rawlins <jermaine.rawlins@gmail.com>
2026-06-30 11:49:17 -0400

    feat(client): add shared types

---
+++ client/src/types.ts
@@ -0,0 +1,31 @@
+export type Feed = 'top' | 'new' | 'best'
+
+export interface Story {
+  id: number
+  title: string
+  by: string
+  score: number
+  time: number
+  descendants: number
+  type: string
+  url?: string
+  text?: string
+}
+
+export interface Comment {
+  id: number
+  by: string
+  time: number
+  text: string
+  replies: Comment[]
+}
+
+export interface StoryDetail {
+  story: Story
+  comments: Comment[]
+}
+
+export interface StorySummary {
+  summary: string
+  commentsUsed: number
+}
